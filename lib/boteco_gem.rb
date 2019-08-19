33cc3e98a486d6c8ac955f01cc134515f614da97
gabriel schiavo <gabriel@lewagon.org>
2019-08-19 12:22:53 -0300

    add some phrases

--- lib/boteco_gem.rb
+++ lib/boteco_gem.rb
@@ -1,5 +1,60 @@
-class boteco_gem
-  def self.hello
-   puts "Minha primeira gem!"
+class BotecoGem
+  def self.quote
+    return [
+      "Persuasivo",
+      "Lutador de sumô não limpa a bunda sozinho.",
+      "Eu acho que quem tatua o rosto não entende como a sociedade funciona.",
+      "Mas se esse computador está ruim, se você colocar Windows vai ficar pior ainda!",
+      "Né...",
+      "Não existe projeto simples, existe Dev sem imaginação.",
+      "Se o código não é simples, não é Ruby.",
+      "Um site nunca fica pronto. Mas você precisa entregar em algum momento.",
+      "E aí você toma um erro no meio de fuça!",
+      "A pessoa escreve Rubens nos Trilhos e desenha uma rodovia.",
+      "Mas não faz sentido o que você está fazendo!",
+      "Poder, poder, você pode, mas não sei porque você faria isso.",
+      "A cópia é o primeiro passo para a criação.",
+      "Não existem limites para a criatividade do ser humano.",
+      "Está um ótimo dia para programar.",
+      "O importante da regra é saber quando quebrar a regra.",
+      "Reboot quem dá é a vida.",
+      "Não faz sentido! Ele quer trocar seis por meia dúzia!!",
+      "Scrum dá para aprender em 15 minutos, rsrsrs",
+      "A vida é muito curta para desperdiçar programando em Java.",
+      "Open Source é a programação mais eficiente que existe"
+    ].sample
+  end
+
+  def self.greetings
+    return [
+      "Opa",
+      "Ei",
+      "Psiu",
+      "Ou",
+      "Fera",
+      "Salve",
+      "If it is not simple, it is not Ruby.",
+      "A site is never ready but you need to deliver it at some point.",
+      "And then you get an error message in your pretty face!",
+      "Dude writes Ruby on RAILS and draws a HIGHWAY.",
+      "It makes no sense what you're doing!",
+      "Can do, you can, you may, but I cannot understand why someone would do it.",
+      "Copy is the first step to creation.",
+      "There are no limits to the creativity of the human being.",
+      "Today is a great day to code.",
+      "The important thing about rules is knowing when to break them",
+      "It's life what gives you the real Reboot.",
+      "It doesn't make sense! He wants to exchange six for a half dozen!!",
+      "It only takes 15 minutes to learn scrum, lol",
+      "Life is too short to waste it coding in Java.",
+      "Open Source is the most efficient software ever"
+    ].sample
+  end
+
+  def self.action
+    return [
+      "Desce mais uma!",
+      "Trás a saidêrá!"
+     ].sample
   end
 end
