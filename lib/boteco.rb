bc4d0b6fff638287b64be2fc452a50457367f79a
gabriel schiavo <gabriel@lewagon.org>
2019-09-10 11:35:18 -0300

    finish action method

--- lib/boteco.rb
+++ lib/boteco.rb
@@ -1,19 +1,6 @@
 class BotecoGem
 
-  def self.action
-    return [
-      "Desce mais uma rodada!",
-      "Desce mais uma",
-      "Traz a saidêrá",
-      "Me vê o supra-sumo",
-      "Solta a gelada",
-      "Traz um refil",
-      "Me dá uma benção!",
-      "Conceda sua graça",
-      "Manda aquela",
-      "Solta já trincando"
-     ].sample
-  end
+
 
   def self.chamarGarcom
     return "#{greetings} meu #{name.downcase}! #{action}!"
---
+++ lib/boteco/action.rb
@@ -0,0 +1,16 @@
+class Boteco::Action
+  def self.action
+    return [
+      "Desce mais uma rodada!",
+      "Desce mais uma",
+      "Traz a saidêrá",
+      "Me vê o supra-sumo",
+      "Solta a gelada",
+      "Traz um refil",
+      "Me dá uma benção!",
+      "Conceda sua graça",
+      "Manda aquela",
+      "Solta já trincando"
+     ].sample
+  end
+end
