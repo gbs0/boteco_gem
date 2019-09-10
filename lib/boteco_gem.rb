bce19f0b4469eee4bac7e1abe898da10d202bcbc
gabriel schiavo <gabriel@lewagon.org>
2019-09-10 11:22:06 -0300

    add new method

--- lib/boteco_gem.rb
+++ lib/boteco_gem.rb
@@ -183,6 +183,10 @@ class BotecoGem
     return "#{greetings} meu #{name.downcase}! #{action}!"
   end
 
+  def self.drunk
+    return "(#{"#{greetings} meu #{name.downcase.reverse} hic! #{action.reverse} hic!"})"
+  end
+
 end
 
-p BotecoGem.chamarGarcom
+p BotecoGem.drunk
