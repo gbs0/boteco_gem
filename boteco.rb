e74660adee15a3e71d2c0bcb295b8015e603a6f3
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-06-30 15:30:52 -0300

    add some refactor code

--- boteco.rb
+++ boteco.rb
@@ -1,21 +1,25 @@
-require_relative 'lib/boteco/actions'
-require_relative 'lib/boteco/greetings'
-require_relative 'lib/boteco/names'
+require 'yaml'
+require 'pry'
+# require_relative 'lib/boteco/actions'
+# require_relative 'lib/boteco/greetings'
+# require_relative 'lib/boteco/names'
 
 class Boteco
+  INFO = YAML.load_file(File.join(__dir__, 'data.yml'))
+  binding.pry
   # Chamar garçom
-  def self.call
-    action = Actions.action
-    greetings = Greetings.greeting
-    names = Names.name
+  def self.chamar
+    action = INFO['pt']['actions']
+    greetings = INFO['pt']['greetings']
+    names = INFO['pt']['names']
     return "#{greetings} meu #{names.downcase}! #{action}!"
   end
 
-  def self.drunk
-    action = Actions.action
-    greetings = Greetings.greeting
-    names = Names.name
-    return "#{"#{greetings} meu #{names.downcase.reverse} hic! #{action.downcase.reverse} hic!"}"
-  end
+  # def self.bebado
+  #   action = Actions.action
+  #   greetings = Greetings.greeting
+  #   names = Names.name
+  #   return "#{"#{greetings} meu #{names.downcase.reverse} hic! #{action.downcase.reverse} hic!"}"
+  # end
 end
 
